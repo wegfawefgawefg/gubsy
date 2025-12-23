80aebb90133ce01c1c2c89fca42362d953ee35ab
wegfawefgawefg <668es218pur@gmail.com>
2025-12-22 21:31:53 -0600

    move the screen ids out of the way

--- src/engine/menu/menu_ids.hpp
+++ src/engine/menu/menu_ids.hpp
@@ -4,7 +4,7 @@
 
 namespace MenuScreenID {
 inline constexpr MenuScreenId NONE = 0;
-inline constexpr MenuScreenId MODS = 2;
-inline constexpr MenuScreenId SETTINGS = 3;
+inline constexpr MenuScreenId MODS = 1u << 20;
+inline constexpr MenuScreenId SETTINGS = (1u << 20) + 1;
 inline constexpr MenuScreenId SETTINGS_CATEGORY_BASE = 1u << 16;
 } // namespace MenuScreenID
