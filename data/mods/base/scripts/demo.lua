ad2ba8321956f1ac0ac85df4d1cfaa7a38a375e4
wegfawefgawefg <668es218pur@gmail.com>
2026-03-26 05:03:55 +0000

    moving shit around

---
+++ data/mods/base/scripts/demo.lua
@@ -0,0 +1,12 @@
+register_demo_content({
+    player = {
+        start = { x = 0.0, y = 0.0 },
+        size = { x = 0.5, y = 0.5 },
+        speed = 4.5,
+    },
+    bonk = {
+        position = { x = 2.5, y = 0.0 },
+        size = { x = 0.75, y = 0.75 },
+        sound = "base:ui_confirm",
+    },
+})
